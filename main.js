9e12d78244cb49c46811f3c608891101cc8cca86
Huy <huyk14tth1@gmail.com>
2022-11-08 10:47:44 +0700

    add code for view from sublist and prev-next button functions

--- main.js
+++ main.js
@@ -50,26 +50,27 @@ const modal = $('.modal');
 const displayedImg = $('.img-display img');
 const slideImgs = $('.slide-imgs');
 const closeBtn = $('.close-btn');
-const nextBtn = $('next-btn');
-const prevBtn = $('prev-btn');
+const nextBtn = $('.next-btn');
+const prevBtn = $('.prev-btn');
+var currentIndex = 0;
 
 function render() {
-    let cityImagePosition = -1;
+    let cityImagePosition = 0;
     let htmlCityImg = imgList.map((img,index)=>{
         if(img.category === 'city'){
             cityImagePosition++;
             // return `<div class="img ${img.category}" style="left:${cityImagePosition*31}rem"><img src="${img.src}" alt=""></div>`;
-            return `<div class="img ${img.category}"><img class="${img.category}" src="${img.src}" alt=""></div>`;
+            return `<div class="img ${img.category}"><img class="${img.category}" id="${cityImagePosition}" src="${img.src}" alt=""></div>`;
  
         }
         
     });
-    let cityNaturePosition = -1;
+    let natureImagePosition = 0;
     let htmlNatureImg = imgList.map((img,index)=>{
         if(img.category === 'nature'){
-            cityNaturePosition++;
+            natureImagePosition++;
             // return `<div class="img ${img.category}" style="left:${cityNaturePosition*31}rem"><img src="${img.src}" alt=""></div>`;
-            return `<div class="img ${img.category}"><img class="${img.category}" src="${img.src}" alt=""></div>`;
+            return `<div class="img ${img.category}"><img class="${img.category}" id="${natureImagePosition}" src="${img.src}" alt=""></div>`;
         }
         
     });
@@ -79,35 +80,17 @@ function render() {
 
 function openImg() {
     const imgs = $$('.img img');
+    
     imgs.forEach((img,index) => {
         img.onclick = () => {
             displayedImg.src = img.src;  
             modal.classList.add('opened');
-            if(img.classList.contains('city')){
-                console.log('city');
-                let cityImagePosition = -1;
-                let htmlCityImg = imgList.map((img,index)=>{
-                    if(img.category === 'city'){
-                        cityImagePosition++;
-                        return `<div><img src="${img.src}" alt=""></div>`;
-                    }
-                });
-                slideImgs.innerHTML = htmlCityImg.join('');
-            }else{
-                console.log('nature');
-                let natureImagePosition = -1;
-                let htmlNatureImg = imgList.map((img,index)=>{
-                    if(img.category === 'nature'){
-                        natureImagePosition++;
-                        return `<div><img src="${img.src}" alt=""></div>`;
-                    }
-                });
-                slideImgs.innerHTML = htmlNatureImg.join('');
-            }
+            displayImgSubList(img);
+            currentIndex = img.id;
+            viewNextImage(currentIndex-1);
+            viewPrevImage(currentIndex-1);
         }
     });
-    
-    closeViewImg();
 }
 
 function closeViewImg(){
@@ -116,11 +99,111 @@ function closeViewImg(){
     }
 }
 
+//function name: displayImgSubList
+//purpose will display a small list of imgs below the main image which has the same category as the main img
+//parameter: image (HTML object)
+function displayImgSubList(image){
+    if(image.classList.contains('city')){
+        let cityImagePosition = 0;
+        let htmlCityImg = imgList.map((img,index)=>{
+            if(img.category === 'city'){
+                cityImagePosition++;
+                if(img.src === image.attributes.src.textContent){
+                    return `<div><img id="${cityImagePosition}" class="selected" src="${img.src}" alt="" onclick="viewImgFromSublist('${img.src}')"></div>`;
+                }else{
+                    return `<div><img id="${cityImagePosition}" src="${img.src}" alt="" onclick="viewImgFromSublist('${img.src}')"></div>`;
 
+                }
+            }
+        });
+        slideImgs.innerHTML = htmlCityImg.join('');
+    }else{
+        let natureImagePosition = 0;
+        let htmlNatureImg = imgList.map((img,index)=>{
+            if(img.category === 'nature'){
+                natureImagePosition++;
+                if(img.src === image.attributes.src.textContent){
+                    return `<div><img id="${natureImagePosition}" class="selected" src="${img.src}" alt="" onclick="viewImgFromSublist('${img.src}')"></div>`;
+                }else{
+                    return `<div><img id="${natureImagePosition}" src="${img.src}" alt="" onclick="viewImgFromSublist('${img.src}')"></div>`;
+
+                }
+            }
+        });
+        slideImgs.innerHTML = htmlNatureImg.join('');
+    }
+}
+
+
+function viewImgFromSublist(imgURL){
+    displayedImg.src = imgURL;
+    const subListImg = $$('.slide-imgs img');
+    subListImg.forEach((img =>{
+        if(img.classList.contains('selected')){
+            img.classList.remove('selected');
+        }else{
+            console.log(img.src, imgURL);
+            if(img.attributes.src.textContent == imgURL){
+                img.classList.add('selected');
+            }
+        }
+    }))
+}
+
+function viewNextImage(currentIndex){
+    nextBtn.onclick = () => {
+        const subListImg = $$('.slide-imgs img');
+        currentIndex++;
+        subListImg.forEach((img =>{
+            if(currentIndex > subListImg.length-1){
+                currentIndex = 0;
+                subListImg[subListImg.length-1].classList.remove('selected');
+                subListImg[currentIndex].classList.add('selected');
+            }
+            if(img.id == currentIndex+1){
+                displayedImg.src = img.attributes.src.textContent;
+                if(currentIndex == 0){
+                    subListImg[currentIndex].classList.add('selected');
+                    subListImg[subListImg.length-1].classList.remove('selected');
+                }else{
+                    subListImg[currentIndex].classList.add('selected');
+                    subListImg[currentIndex-1].classList.remove('selected');
+                }
+                
+            }
+        }))
+    }
+}
+
+function viewPrevImage(currentIndex){
+    prevBtn.onclick = () => {
+        const subListImg = $$('.slide-imgs img');
+        currentIndex--;
+        subListImg.forEach((img =>{
+            if(currentIndex < 0){
+                currentIndex = subListImg.length-1;
+                subListImg[0].classList.remove('selected');
+                subListImg[currentIndex].classList.add('selected');
+            }
+            if(img.id == currentIndex+1){
+                displayedImg.src = img.attributes.src.textContent;
+                if(currentIndex == 0){
+                    subListImg[currentIndex].classList.remove('selected');
+                    subListImg[subListImg.length-1].classList.add('selected');
+                }else{
+                    subListImg[currentIndex].classList.remove('selected');
+                    subListImg[currentIndex-1].classList.add('selected');
+                }
+            }
+        }))
+
+    }
+}
 
 function start() {
     render();
     openImg();
+    closeViewImg();
 }
 
 start()
